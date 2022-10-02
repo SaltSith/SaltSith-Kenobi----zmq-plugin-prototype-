8da72e8cab4f8401435b6153228e47150355b03f
SaltSith66 <SaltSith66@gmail.com>
2022-10-02 14:38:05 +0200

    keep zmq_plugin_task queue name in config

--- config.h
+++ config.h
@@ -2,6 +2,8 @@
 
 #include "zmq_plugin/zmq_plugin_socket/zmq_plugin_socket.h"
 
+#define zmq_plugin_task_QUEUE_NAME  "/zmq_plugin_kenobi_queue"
+
 #define PLUGIN_ADDR "tcp://*:5558"
 #define PLUGIN_ROLE RESPONDER
 
