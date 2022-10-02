beb9e06c4289c5296ce0808b16a7dbf576108479
SaltSith66 <SaltSith66@gmail.com>
2022-10-02 12:26:59 +0200

    update return values

--- plugin/plugin_socket/plugin_socket.h
+++ plugin/plugin_socket/plugin_socket.h
@@ -8,8 +8,8 @@ typedef enum {
 
 int plugin_socket_init(const socket_role_t role, const char *addr);
 
-void plugin_socket_destroy(void);
+int plugin_socket_destroy(void);
 
-void plugin_socket_send_message(const char *message, const int len);
+int plugin_socket_send_message(const char *message, const int len);
 
 void *plugin_socket_get(void);
